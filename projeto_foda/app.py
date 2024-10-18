c387b56f10ad676217480622911da37e8e138af7
Sol Aniel <129297452+Sol-Aniel@users.noreply.github.com>
2024-10-18 11:19:39 -0300

    Update app.py

--- projeto_foda/app.py
+++ projeto_foda/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for, request, session
+from flask import Flask, render_template, redirect, url_for, request, flash, abort
 from controllers.controller import sol_controller
 from controllers.login import login_control
 
