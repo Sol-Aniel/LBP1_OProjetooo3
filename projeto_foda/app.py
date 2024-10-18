7e4f14695d175deefcb6f67e6b34eb50c47f866e
Sol Aniel <129297452+Sol-Aniel@users.noreply.github.com>
2024-10-17 21:41:09 -0300

    Update app.py

--- projeto_foda/app.py
+++ projeto_foda/app.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, session
 from controllers.controller import sol_controller
+from controllers.login import login_control
+
 app = Flask(__name__)
+app.secret_key = 'chave_screta'
 app.register_blueprint(sol_controller)
+app.register_blueprint(login_control)
 
 if __name__ == "__main__":
     app.run()
