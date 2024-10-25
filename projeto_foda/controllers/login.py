1bc866f4c4f916b89ad2c0ef289b1950c07bc8f1
Sol Aniel <129297452+Sol-Aniel@users.noreply.github.com>
2024-10-25 17:15:16 -0300

    Update login.py

--- projeto_foda/controllers/login.py
+++ projeto_foda/controllers/login.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request, session, flash, abort
+from flask import Blueprint, Response, render_template, redirect, url_for, request, session, flash, abort, make_response
 from models.usuarios import *
 
 login_control = Blueprint('login',__name__)
@@ -19,18 +19,37 @@ def login():
     user = request.form["username"]
     key = request.form["senha"]
     if verificarUsuario(user, key)==True:
-        flash('Login efetuado', 'alert-success')
+        flash('Login efetuado', 'success')
         session['username'] = request.form["username"]
         return redirect(url_for('login.dash'))
     else:
-        flash('Usuário não encontrado ou senha incorreta', 'alert-error')
+        flash('Usuário não encontrado ou senha incorreta', 'danger')
         return render_template("login.html")
     
 @login_control.route('/painel')
 def dash():
-    return render_template("painel.html", username = session['username'])
-    
+    if verificarAdmin(session['username'])==True:
+        return render_template("painel.html", username = session['username'])
+    else:
+        return render_template("compras.html", username = session['username'], carrinho = session['carrinho'])
+
+@login_control.route('/painel', methods=["POST", "GET"])
+def carrinho():
+    if verificarAdmin(session['username'])==True:
+        return render_template("painel.html", username = session['username'])
+    else:
+        frutas = request.form["frutas"]
+        quant =request.form["quantidade"]
+        r = make_response("Cookie has been set!")
+        q = make_response("Cookie has been set!")
+        r.set_cookie(frutas, frutas, max_age=60*60)
+        q.set_cookie((frutas + 'q'), quant, max_age=60*60)
+        adicionarCarrinho(request.cookies.get(frutas), request.cookies.get(frutas + 'q'))
+        return render_template("compras.html", username = session['username'], carrinho = carrinho)
+ 
+
 @login_control.route('/logout', methods=['POST'])
 def deslogar():
     session.pop('username', None)
+    session.pop('carrinho', None)
     return redirect(url_for('login.index'))
